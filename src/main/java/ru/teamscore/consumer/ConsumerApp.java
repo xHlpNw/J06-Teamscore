ce5b393cb6ca8f869165a9e982cfc2ea76516e7c
xHlpNw <aleksashin990@gmail.com>
2025-12-29 11:48:01 +0400

    ConsumerApp

---
+++ src/main/java/ru/teamscore/consumer/ConsumerApp.java
@@ -0,0 +1,8 @@
+package ru.teamscore.consumer;
+
+public class ConsumerApp {
+    public static void main(String[] args) {
+        ConsumerService service = new ConsumerService();
+        service.run();
+    }
+}
