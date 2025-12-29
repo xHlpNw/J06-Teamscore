d43ca521754686d2c0618bdc7592f492829f29d1
xHlpNw <aleksashin990@gmail.com>
2025-12-29 09:46:27 +0400

    JsonParser class

---
+++ src/test/java/consumer/JsonParser.java
@@ -0,0 +1,16 @@
+package consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonParser {
+    private final static ObjectMapper MAPPER = new ObjectMapper();
+
+    public static <T> T parse(String json, Class<T> clazz) {
+        try {
+            return MAPPER.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Invalid json: " + json);
+        }
+    }
+}
