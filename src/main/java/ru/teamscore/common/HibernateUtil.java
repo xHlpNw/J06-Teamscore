30bd48519c456b5f95af065698dc9bb738a9a429
xHlpNw <aleksashin990@gmail.com>
2025-12-29 11:13:23 +0400

    SensorDataPersister class

--- src/main/java/ru/teamscore/common/HibernateUtil.java
+++ src/main/java/ru/teamscore/common/HibernateUtil.java
@@ -2,8 +2,7 @@ package ru.teamscore.common;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import ru.teamscore.common.entity.ProcessedMessage;
-import ru.teamscore.common.entity.SensorMessage;
+import ru.teamscore.common.entity.*;
 
 public class HibernateUtil {
     private static final SessionFactory SESSION_FACTORY = build();
@@ -13,6 +12,10 @@ public class HibernateUtil {
             return new Configuration()
                     .addAnnotatedClass(SensorMessage.class)
                     .addAnnotatedClass(ProcessedMessage.class)
+                    .addAnnotatedClass(LightData.class)
+                    .addAnnotatedClass(BarometerData.class)
+                    .addAnnotatedClass(LocationData.class)
+                    .addAnnotatedClass(AccelerometerData.class)
                     .buildSessionFactory();
         } catch (Exception e) {
             throw new ExceptionInInitializerError(e);
